f7647368e02cc4f971a99c49255fd2633d7ce84d
KendrickPC <kennethpchang@gmail.com>
2016-11-29 17:44:27 +0800

    associative array setup

--- addressBook.js
+++ addressBook.js
@@ -1,1 +1,12 @@
-addressBook.js
+// stored in an associative array with a property and value
+
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(408) 777-7777",
+    email: "bob.jones@gmail.com"
+};
+
+console.log(bob.firstName);
+console.log();
+console.log();
