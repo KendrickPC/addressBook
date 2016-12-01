b0a430efe1ce98e1fafac07f17b514d6b334839c
KendrickPC <kennethpchang@gmail.com>
2016-12-01 13:39:04 +0800

    printPerson function testing

--- addressBook.js
+++ addressBook.js
@@ -1,19 +1,23 @@
-// stored in an associative array with a property and value
-
 var bob = {
     firstName: "Bob",
     lastName: "Jones",
-    phoneNumber: "(408) 777-7777",
-    email: "bob.jones@gmail.com"
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
 };
 
-console.log(bob.firstName);
-console.log(bob.lastName);
-console.log(bob.email);
-
 var mary = {
-	firstName: "Mary",
-	lastName: "Johnson",
+    firstName: "Mary",
+    lastName: "Johnson",
     phoneNumber: "(650) 888-8888",
     email: "mary.johnson@example.com"
-}
+};
+
+var contacts = [bob, mary];
+
+var printPerson = function(person) {
+    console.log(person.firstName + " " + person.lastName)
+};
+
+printPerson(contacts[0]);
+printPerson(contacts[1]);
+
